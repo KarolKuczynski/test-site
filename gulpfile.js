654c3bdaf3b343e36d2b593bcdc38d403cddb64f
karolkuczynski82 <karolkuczynski@o2.pl>
2018-04-19 22:24:24 +0200

    added more css files

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("Gulp Task");
@@ -14,9 +15,8 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-    //return gulp.src('./app/assets/styles/styles.css')
-    return gulp.src('./app/assets/styles/**/*.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
