82313bfb11c3c827dd883646d1c3677e2a83dcf6
karolkuczynski82 <karolkuczynski@o2.pl>
2018-04-24 18:20:16 +0200

    added babel configuration

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,15 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+//var Person = require('./modules/Person');
+import Person from './modules/Person';
 
-var john = new Person('John','blue');
+class Adult extends Person {
+    payTax() {
+        console.log(this.name + " now owes $0 in taxes.")
+    }
+}
+
+var john = new Adult('John','blue');
 john.greet();
+john.payTax();
 
 //$('h1').remove();
